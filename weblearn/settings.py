2dce1d01ae6fba3b536d7c06ff22c338414eb874
Amir Mohamad Hoseini Nezhad <amirmhmd.hosini@gmail.com>
2026-05-20 15:26:44 +0330

    static test

--- weblearn/settings.py
+++ weblearn/settings.py
@@ -117,3 +117,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'static'
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
+STATICFILES_DIR = [
+    BASE_DIR / 'statics',
+]
