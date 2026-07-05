a5b7bee1aee6ef64518d36248d85a8c1ffb7a80f
Amir Mohamad Hoseini Nezhad <amirmhmd.hosini@gmail.com>
2026-07-05 16:21:33 +0330

    dynamic urls

--- apptest/urls.py
+++ apptest/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from apptest.views import *
 
 urlpatterns = [
-    path('', index_view ),
-    path('about/', about_view),
-    path('contact/', contact_view),
-    path('blog-single/', blog_single_view),
-    path('blog-home/', blog_home_view),
+    path('', index_view, name = 'home' ),
+    path('about/', about_view, name = 'about'),
+    path('contact/', contact_view, name = 'contact'),
+    path('blog-single/', blog_single_view, name = 'blog-single'),
+    path('blog-home/', blog_home_view, name = 'blog-home'),
 ]
