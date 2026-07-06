5cec931685feb545f2d8ffe4c8bebe6355c6ec97
Amir Mohamad Hoseini Nezhad <amirmhmd.hosini@gmail.com>
2026-07-06 21:08:56 +0330

    dynamic blog test

--- blog/views.py
+++ blog/views.py
@@ -7,4 +7,8 @@ def blog_view(request):
     return render(request, 'blog-home.html')
 
 def blog_single(request):
-    return render(request, 'blog-single.html')
+    context = {'title' : 'Dynamic Blog',
+               'content': 'im just trying to make my first dynamic blog and im trying some new stuff :) ',
+               'author' : 'amir mhmd'
+               }
+    return render(request, 'blog-single.html', context)
