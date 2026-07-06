955ced93c3312f387112ab23c75f3eb7d53db79a
Amir Mohamad Hoseini Nezhad <amirmhmd.hosini@gmail.com>
2026-07-06 20:47:21 +0330

    tesat

--- apptest/urls.py
+++ apptest/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', index_view, name = 'home' ),
     path('about/', about_view, name = 'about'),
     path('contact/', contact_view, name = 'contact'),
-    path('blog-single/', blog_single_view, name = 'blog-single'),
-    path('blog-home/', blog_home_view, name = 'blog-home'),
+    path('test/', test_view, name = 'test'),
+
+    
 ]
--- apptest/views.py
+++ apptest/views.py
@@ -10,8 +10,5 @@ def contact_view(request):
 def about_view(request):
     return render(request, 'about.html')
 
-def blog_home_view(request):
-    return render(request, 'blog-home.html')
-
-def blog_single_view(request):
-    return render(request, 'blog-single.html')
+def test_view(request):
+    return render(request, 'test.html')
