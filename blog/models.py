7ee0ea9dd6675eff791c44b090420f1dd9257f24
Amir Mohamad Hoseini Nezhad <amirmhmd.hosini@gmail.com>
2026-07-15 14:00:41 +0330

    complete models.py
    added post and contact class in models

--- blog/models.py
+++ blog/models.py
@@ -4,4 +4,12 @@ from django.db import models
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
-    
+    # image
+    # author
+    # tag
+    # category
+    counted_views = models.IntegerField(default=0)
+    is_published = models.BooleanField(default=False)
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+    published_date = models.DateTimeField(null=True)
