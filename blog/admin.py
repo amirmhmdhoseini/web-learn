aceb2a212d2abb118fcb93f46dcee7d18dece487
Amir Mohamad Hoseini Nezhad <amirmhmd.hosini@gmail.com>
2026-07-16 13:26:25 +0330

    customize Post Admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+class PostAdmin (admin.ModelAdmin):
+    date_hierarchy = 'created_date'
+    # fields = ('title', 'content')
+    # exclude = ('title',)
+    list_display = ('title', 'is_published', 'counted_views', 'created_date')
+    # ordering = ['-created_date']
+    search_fields = ['title']
+    list_filter = ('is_published', 'created_date')
+admin.site.register(Post, PostAdmin)
