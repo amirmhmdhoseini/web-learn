697d8530f9b8f7a2f24ec97528d189c338f215fd
Amir Mohamad Hoseini Nezhad <amirmhmd.hosini@gmail.com>
2026-07-19 17:03:09 +0330

    added author field

--- blog/admin.py
+++ blog/admin.py
@@ -6,8 +6,8 @@ class PostAdmin (admin.ModelAdmin):
     date_hierarchy = 'created_date'
     # fields = ('title', 'content')
     # exclude = ('title',)
-    list_display = ('title', 'is_published', 'counted_views', 'created_date')
+    list_display = ('title','author', 'is_published', 'counted_views', 'created_date')
     # ordering = ['-created_date']
     search_fields = ['title']
-    list_filter = ('is_published', 'created_date')
+    list_filter = ('is_published', 'created_date', 'author')
 admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=255)
     content = models.TextField()
     # image
-    # author
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     # tag
     # category
     counted_views = models.IntegerField(default=0)
