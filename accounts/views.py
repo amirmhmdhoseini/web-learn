05851ec536e9da4b3540637af10214e7f1fd97b5
Amir Mohamad Hoseini Nezhad <amirmhmd.hosini@gmail.com>
2026-08-09 23:15:20 +0330

    logout
    added logout view and login required decorator

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render,redirect
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def login_view(request):
@@ -22,7 +23,10 @@ def login_view(request):
     context = {'form' : form}
     return render(request, 'accounts/login.html', context)    
 
-# logout
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('/')
 
 def signup_view(request):
     return render(request, 'accounts/signup.html')
