ac73564a0ddb8d0fc3d85c7d0e035c7c0b10da3d
Amir Mohamad Hoseini Nezhad <amirmhmd.hosini@gmail.com>
2026-08-15 18:43:58 +0330

    email
    aadded email for forgot password

--- weblearn/settings.py
+++ weblearn/settings.py
@@ -133,5 +133,15 @@ MULTI_CAPTCHA_ADMIN = {
 }
 
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'amirmhmd.hosini@gmail.com'
+EMAIL_HOST_PASSWORD = 'oddiotguldtohvdb'
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
